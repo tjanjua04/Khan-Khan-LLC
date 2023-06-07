057806dc2eda0e83995035d72c2de81f20a6eb4f
tjanjua04 <71242006+tjanjua04@users.noreply.github.com>
2023-06-06 23:06:05 -0700

    Update psudeocode.cpp

--- psudeocode.cpp
+++ psudeocode.cpp
@@ -1,26 +1,17 @@
-// list procedures
-// how to check the user input
-// any conditions or loops
-// outline of code
-// put into number list
-// list-type level when using psuedocode
-
 /* include statements
+include <iostream>
+include <string>
+includ <ctime>
 
-#include <iostream>
-#include <string>
-
-  variable of # of languages = 5.0;
+  variable of # of languages = 5.0
   
-  variable of # of countries = 10.0;
+  variable of # of countries = 10.0
   
   // 5 languages for 10 countries
   
-  countries[5][10] = { {English speaking countries}, {Spanish speaking countries}, {Urdu speaking countries}, {Russian speaking countries}, {Arabic speaking countries}
+  make a countries array with 5 languages, every 5 languages has 10 countries
   
-  // integers for the input values
-  int languageChoice;
-	int specialtyChoice;
+  // make integers for the input values that will store these values for the case
 
   // case that asks the user what their prefered language is
 
@@ -32,6 +23,5 @@
          
   }
 
-  // sample size
-  The sample space is the amount of languages multiplied by the amount of countries. 5 * 10 = 50  
+  // output statement that says the sample size which is the 5 languages and the 10 countries multipled
 } */
